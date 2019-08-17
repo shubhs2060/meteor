1d36d8faf271f1cd4a7024a1fbfa3f5bcc0d4ff5
shubhs2060 <shubhamsrivastava2050>
2019-08-17 22:01:26 +0530

    Add files

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import List from './List.jsx';
 
 export default class Hello extends Component {
   state = {
     counter: 0,
-    points: 0
+    points: 0,
+    test: 0,
+    best: false,
+    places: [23,4,3,4,3434]
   }
 
   increment() {
@@ -13,17 +17,38 @@ export default class Hello extends Component {
   }
 
   updatePoint() {
-    this.setState({points: points + 32})
+    this.setState({points: this.state.points + 32})
+  }
+
+  showMessage() {
+    console.log("Show a Message")
+    this.setState({test: this.state.test + 90,best: true})
   }
 
   render() {
     var example = "ex variable"
+    let place = null;
+    place = (
+      <div>
+        <p>in loop</p>
+        {this.state.places.map(p => {
+          
+          return <List name={p} />
+        })}
+      </div>
+    );
+    
     return (
       <div>
+        {place}
         <button onClick={() => this.increment()}>Click Me</button>
         <p>You've pressed the button {this.state.counter} times.</p>
         <p>Ax{example}</p>
-
+        <button onClick={() => this.showMessage()}> Show Message</button>
+        <p>{this.state.test}</p>
+        <a onClick={() => this.updatePoint()}>asd</a>
+        <p>{this.state.points}</p>
+        { this.state.best ? <p>terafa </p> : <p>toast </p>}
       </div>
     );
   }
--- imports/ui/Rclass.jsx
+++ imports/ui/Rclass.jsx
@@ -1,15 +1,12 @@
 import React,{Component} from 'react';
 
-export default class Rclass extends Component {
-
-    
-        render(){
-            return (
-                <div>
-                    <h2>Wanna React Class component</h2>
-                    <p>V  class State and Props</p>
-                </div>
-            );
-        }
-    
+export default class Rclass extends Component {   
+    render(){
+        return (
+            <div>
+                <h2>Wanna React Class component</h2>
+                <p>V  class State and Props</p>
+            </div>
+        );
+    }
 }
